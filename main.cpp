18b5b3c02ec244ec2c0c653e42d58017449546bf
Nico <nicolasdortu@outlook.com>
2024-06-15 11:05:07 +0200

    Added input boxes for edit config

--- main.cpp
+++ main.cpp
@@ -3,24 +3,29 @@
 #include <string>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK MacroDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 void AddControls(HWND hwnd);
 void LoadConfiguration();
 void SaveConfiguration();
 void ExecuteMacro(const std::string &command);
 void RegisterHotKeys(HWND hwnd);
+void ShowMacroInputDialog(HWND hwnd, int buttonId);
 
-HWND hButton1, hButton2, hButtonSave;
-HWND hEdit1, hEdit2;
+HWND hButton1, hButton2, hButtonEdit1, hButtonEdit2;
 
 std::string button1Macro;
 std::string button2Macro;
 
 #define ID_HOTKEY1 1
 #define ID_HOTKEY2 2
+#define IDC_MACRO_EDIT 1001
+#define IDC_BUTTON_SAVE 1002
+#define IDC_BUTTON_CANCEL 1003
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
   const char CLASS_NAME[] = "MacroBoxWindowClass";
+  const char DIALOG_CLASS_NAME[] = "MacroDialogClass";
 
   // Register the window class
   WNDCLASS wc = {};
@@ -32,13 +37,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
   RegisterClass(&wc);
 
+  // Register the dialog class
+  WNDCLASS dialogWc = {};
+
+  dialogWc.lpfnWndProc = MacroDialogProc;
+  dialogWc.hInstance = hInstance;
+  dialogWc.lpszClassName = DIALOG_CLASS_NAME;
+  dialogWc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+  RegisterClass(&dialogWc);
+
   // Create the window
   HWND hwnd = CreateWindowEx(
       0,
       CLASS_NAME,
       "Macro Box",
       WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, CW_USEDEFAULT, 500, 400,
+      CW_USEDEFAULT, CW_USEDEFAULT, 350, 400,
       NULL,
       NULL,
       hInstance,
@@ -83,7 +98,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     return 0;
 
   case WM_COMMAND:
-    switch (wParam)
+    switch (LOWORD(wParam))
     {
     case 1:
       ExecuteMacro(button1Macro);
@@ -91,9 +106,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     case 2:
       ExecuteMacro(button2Macro);
       break;
-    case 3:
-      SaveConfiguration();
-      MessageBox(hwnd, "Configuration saved!", "Info", MB_OK);
+    case 101:
+      ShowMacroInputDialog(hwnd, 1);
+      break;
+    case 102:
+      ShowMacroInputDialog(hwnd, 2);
       break;
     }
     return 0;
@@ -135,6 +152,16 @@ void AddControls(HWND hwnd)
       (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
       NULL);
 
+  hButtonEdit1 = CreateWindow(
+      "BUTTON",
+      "Edit",
+      WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+      210, 50, 50, 50,
+      hwnd,
+      (HMENU)101,
+      (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+      NULL);
+
   hButton2 = CreateWindow(
       "BUTTON",
       "Button 2",
@@ -145,33 +172,13 @@ void AddControls(HWND hwnd)
       (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
       NULL);
 
-  hButtonSave = CreateWindow(
+  hButtonEdit2 = CreateWindow(
       "BUTTON",
-      "Save Configuration",
+      "Edit",
       WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-      50, 250, 150, 50,
+      210, 150, 50, 50,
       hwnd,
-      (HMENU)3,
-      (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
-      NULL);
-
-  hEdit1 = CreateWindow(
-      "EDIT",
-      button1Macro.c_str(),
-      WS_TABSTOP | WS_VISIBLE | WS_CHILD | WS_BORDER | ES_LEFT,
-      250, 50, 200, 20,
-      hwnd,
-      NULL,
-      (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
-      NULL);
-
-  hEdit2 = CreateWindow(
-      "EDIT",
-      button2Macro.c_str(),
-      WS_TABSTOP | WS_VISIBLE | WS_CHILD | WS_BORDER | ES_LEFT,
-      250, 150, 200, 20,
-      hwnd,
-      NULL,
+      (HMENU)102,
       (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
       NULL);
 }
@@ -189,14 +196,6 @@ void LoadConfiguration()
 
 void SaveConfiguration()
 {
-  char buffer[256];
-
-  GetWindowText(hEdit1, buffer, 256);
-  button1Macro = buffer;
-
-  GetWindowText(hEdit2, buffer, 256);
-  button2Macro = buffer;
-
   std::ofstream config("macros.cfg");
   if (config.is_open())
   {
@@ -210,9 +209,6 @@ void ExecuteMacro(const std::string &command)
 {
   if (!command.empty())
   {
-    // Show the command in a message box for debugging purposes
-    MessageBox(NULL, command.c_str(), "Executing Command", MB_OK);
-
     // Execute the command
     system(command.c_str());
   }
@@ -220,4 +216,92 @@ void ExecuteMacro(const std::string &command)
   {
     MessageBox(NULL, "No command entered.", "Error", MB_OK);
   }
+}
+
+void ShowMacroInputDialog(HWND hwnd, int buttonId)
+{
+  HINSTANCE hInstance = (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE);
+
+  HWND hDlg = CreateWindowEx(
+      WS_EX_DLGMODALFRAME,
+      "MacroDialogClass",
+      "Edit Macro",
+      WS_VISIBLE | WS_CAPTION | WS_SYSMENU | WS_POPUP | WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, CW_USEDEFAULT, 250, 150,
+      hwnd,
+      NULL,
+      hInstance,
+      NULL);
+
+  if (!hDlg)
+  {
+    MessageBox(hwnd, "Failed to create dialog", "Error", MB_OK);
+    return;
+  }
+
+  std::string *currentMacro = (buttonId == 1) ? &button1Macro : &button2Macro;
+
+  HWND hEditMacro = CreateWindow(
+      "EDIT",
+      currentMacro->c_str(),
+      WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT | ES_AUTOHSCROLL,
+      20, 20, 200, 20,
+      hDlg,
+      (HMENU)IDC_MACRO_EDIT,
+      hInstance,
+      NULL);
+
+  HWND hButtonSave = CreateWindow(
+      "BUTTON",
+      "Save",
+      WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON,
+      50, 60, 60, 25,
+      hDlg,
+      (HMENU)IDC_BUTTON_SAVE,
+      hInstance,
+      NULL);
+
+  HWND hButtonCancel = CreateWindow(
+      "BUTTON",
+      "Cancel",
+      WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+      130, 60, 60, 25,
+      hDlg,
+      (HMENU)IDC_BUTTON_CANCEL,
+      hInstance,
+      NULL);
+
+  SetWindowLongPtr(hDlg, GWLP_USERDATA, (LONG_PTR)currentMacro);
+
+  ShowWindow(hDlg, SW_SHOW);
+}
+
+LRESULT CALLBACK MacroDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+  std::string *currentMacro = (std::string *)GetWindowLongPtr(hDlg, GWLP_USERDATA);
+
+  switch (message)
+  {
+  case WM_COMMAND:
+    switch (LOWORD(wParam))
+    {
+    case IDC_BUTTON_SAVE:
+    {
+      char buffer[256];
+      GetWindowText(GetDlgItem(hDlg, IDC_MACRO_EDIT), buffer, 256);
+      *currentMacro = buffer;
+      SaveConfiguration();
+      DestroyWindow(hDlg);
+      return (INT_PTR)TRUE;
+    }
+    case IDC_BUTTON_CANCEL:
+      DestroyWindow(hDlg);
+      return (INT_PTR)TRUE;
+    }
+    break;
+  case WM_CLOSE:
+    DestroyWindow(hDlg);
+    return (INT_PTR)TRUE;
+  }
+  return DefWindowProc(hDlg, message, wParam, lParam);
 }
---
+++ resource.h
@@ -0,0 +1,2 @@
+#define IDD_MACRO_DIALOG 101
+#define IDC_MACRO_EDIT 1001
